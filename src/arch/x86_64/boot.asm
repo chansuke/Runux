2168f230e3cf02ca129b5dafd5c0f0453850492c
chansuke <moonset20@gmail.com>
2016-07-23 12:53:38 +0000

    Add page tables

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -27,6 +27,20 @@ check_multiboot:
     mov al, "0"
     jmp error
 
+set_up_page_tables:
+    ; map first P4 entry to P3 table
+    mov eax, p3_table
+    or eax, 0b11 ; present + writable
+    mov [p4_table], eax
+
+    ; map first P3 entry to P2 table
+    mov eax, p2_table
+    or eax, 0b11 ; present + writable
+    mov [p3_table], eax
+
+    ; TODO map each P2 entry to a huge 2MiB page
+    ret
+
 check_cpuid:
     ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
     ; in the FLAGS register. If we can flip it, CPUID is available.
@@ -81,6 +95,13 @@ check_long_mode:
     jmp error
 
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
     resb 64
 stack_top:
