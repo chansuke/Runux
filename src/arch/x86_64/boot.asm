e174254f3bddc15fd36b4fec7e09a99e914e8057
chansuke <moonset20@gmail.com>
2016-07-23 13:42:04 +0000

    Load GDT

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,16 +1,24 @@
 global start
 
 section .text
+bits 32
 start:
-		mov  esp, stack_top
-		
-  		call check_multiboot
-		call check_cpuid
-		call check_long_mode
+    mov  esp, stack_top
+    
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+    
+    ; pagination
+    call set_up_page_tables
+    call enable_paging
 
-		; print 'OK'
-		mov dword [0xb8000], 0x2f4b2f4f
-		hlt
+    ; load the 64-bit GDT
+    lgdt [gdt64.pointer]
+    
+    ; print 'OK'
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
 
 error:
     mov dword [0xb8000], 0x4f524f45
@@ -30,7 +38,7 @@ check_multiboot:
 set_up_page_tables:
     ; map first P4 entry to P3 table
     mov eax, p3_table
-    or eax, 0b11 ; present + writable
+    or eax, 0b11 
     mov [p4_table], eax
 
     ; map first P3 entry to P2 table
@@ -38,7 +46,42 @@ set_up_page_tables:
     or eax, 0b11 ; present + writable
     mov [p3_table], eax
 
-    ; TODO map each P2 entry to a huge 2MiB page
+    ; map each P2 entry to a huge 2MiB page
+    mov ecx, 0
+
+.map_p2_table:
+    mov eax, 0x200000  ; 2MiB
+    mul ecx            ; start address of ecx-th page
+    or eax, 0b10000011 ; present + writable + huge
+    mov [p2_table + ecx * 8], eax ; map ecx-th entry
+
+    inc ecx            ; increase counter
+    cmp ecx, 512       ; if counter == 512, the whole P2 table is mapped
+    jne .map_p2_table  ; else map the next entry
+
+    ret
+
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
     ret
 
 check_cpuid:
@@ -105,3 +148,13 @@ p2_table:
 stack_bottom:
     resb 64
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+    dq (1<<44) | (1<<47) | (1<<41) ; data segment
+    dq (1<<44) | (1<<47) | (1<<41)
+pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
